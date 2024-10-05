1426ea3e18141c9b50a2f03801224dc2a823a765
Mateus Evangelista <enzoteuprogrammer@gmail.com>
2024-10-05 15:33:54 -0300

    Seção JS Assíncrono - Save

--- JavaScript/assincrono/03_asyncAwait.js
+++ JavaScript/assincrono/03_asyncAwait.js
@@ -12,14 +12,12 @@ function esperaAi(msg, tempo) {
         reject('Caí no erro!');
         return
       }
-      
+
       resolve(msg.toUpperCase());
       return
     }, tempo);
   });
 }
-
-
 
 /* esperaAi('Fase 1', rand())
   .then(valor => {
@@ -31,4 +29,36 @@ function esperaAi(msg, tempo) {
   }).then(valor => {
     console.log(valor);
   }).catch(e => console.log(e));
-*/
+*/ 
+
+async function executa() {
+  try {
+    // Se tirarmos a `await`, o console retornará uma PROMISE pendente, pois, não estamos esperando ela ser concluída.
+    const fase1 = esperaAi('Fase 1', rand());
+    console.log(fase1);
+
+    // pegando o valor da promise que está pendente
+    setTimeout(function() {
+      console.log(fase1);
+    }, 5000);
+  
+    const fase2 = await esperaAi('Fase 2', rand());
+    console.log(fase2);
+    
+    const fase3 = await esperaAi('Fase 3', rand());
+    console.log(fase3);
+    
+    console.log('Terminamos na fase:', fase3);
+  } catch(e) {
+    // Desta forma, adicionamos um catch.
+    console.log(e);
+  }
+}
+
+executa();
+
+/** *** Estados de uma PROMISE
+ * Pendente (pending) - Foi executada e ainda não retornou o valor.
+ * fullfilled -> Reperesentação de sucesso
+ * rejected -> Rejeitada
+ */
