1ebb4d664b67fde8192bd800671470a615e15545
Mateus Evangelista <enzoteuprogrammer@gmail.com>
2024-10-19 11:13:40 -0300

    Seção JS Node - Save

---
+++ JavaScript/node/express-injetando-conteudo/server.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+
+const express = require("express");
+const app = express();
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.CONNECTIONSTRING)
+  .then(() => {
+    app.emit('Pronto');
+  }).catch((e) => {
+    console.log(e);
+  });
+
+const session = require('express-session');
+const mongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+
+const path = require("path");
+const routes = require("./routes");
+const meuMiddleware = require("./src/middlewares/middleware");
+const MongoStore = require('connect-mongo');
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.resolve(__dirname, "public")));
+
+const sessionOptions = session({
+  secret: 'kaoi39i492034i2901',
+  // store: new MongoStore({ mongooseConnection: monsoge.connection }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: true
+  },
+  store: mongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING })
+});
+
+app.use(sessionOptions);
+app.use(flash());
+
+app.set("views", path.resolve(__dirname, "src", "views"));
+app.set("view engine", "ejs");
+
+// Nossos próprios middlewares
+app.use(meuMiddleware);
+app.use(routes);
+
+app.on('Pronto', () => {
+  app.listen(3000, () => {
+    console.log("Acessar http://localhost:3000");
+    console.log("Servidor executando na porta 3000");
+  });
+});
+
