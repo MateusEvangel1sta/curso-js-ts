5da0b7e8eaba9d7028f672b9f0924aa8c115ffcd
Mateus Evangelista <enzoteuprogrammer@gmail.com>
2024-10-26 13:05:37 -0300

    Seção Básica TS - Save

---
+++ TypeScript/src/tiposBasicos/12_tiposLiterais.ts
@@ -0,0 +1,14 @@
+let x = 10; // tipo: number
+x = 0b1010;
+
+const y = 10; // tipo: 10
+
+let a = 100 as const; // eslint-disable-line
+
+const pessoa = {
+  nome: 'Luiz' as const,
+  sobrenome: 'Miranda',
+};
+
+// Module mode
+export default 1;
