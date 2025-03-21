92bc985d9410da82badf8c44e3a82abfa7ea1ceb
Mateus Evangelista <enzoteuprogrammer@gmail.com>
2025-03-21 20:27:38 -0300

    Curso JavaScript - Seção Iniciantes -- 07

---
+++ JavaScript/Basico/12_array.js
@@ -0,0 +1,20 @@
+const alunos = ['Luiz', 'Maria', 'João'];
+console.log(alunos);
+
+console.log(alunos[0]);
+
+alunos[2] = 'Eduado';
+console.log(alunos);
+
+alunos[3] = 'Luiza';
+console.log(alunos);
+
+console.log(alunos.length);
+
+alunos.push('Fábio');
+console.log(alunos);
+
+alunos.unshift('Carolina');
+console.log(alunos);
+
+// continue
